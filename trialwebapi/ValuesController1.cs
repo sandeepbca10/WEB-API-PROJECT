18d87d3f68a071c390adb6211cf049b3a89a06bc
CEG-PC <91880@CEG-PC>
2024-05-31 07:25:33 +0530

    Add project files.

---
+++ trialwebapi/ValuesController1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Data;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace trialwebapi
+{
+    public class PController : ApiController
+    {
+
+
+
+
+
+
+
+
+        // GET api/<controller>
+        //public IEnumerable<Employee> Get()
+        /*public IHttpActionResult Get()   
+        {
+            List<Employee> employees = new List<Employee>();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["smsystemconn"].ConnectionString;
+            string query = "SELECT * FROM user1";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Employee employee = new Employee();
+                        employee.Id = reader.GetInt32(0);
+                        employee.Name = reader.GetString(1);
+                        // Add more properties as needed
+
+                        employees.Add(employee);
+                    }
+                }
+            }
+
+            return Ok(employees);// for Json Retrun
+            //return employees; for html retrun
+
+        }*/
+
+        public IHttpActionResult Get()
+        {
+            List<Employee> employees = new List<Employee>();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["smsystemconn"].ConnectionString;
+            //string query = "SELECT studname FROM stud_master";
+            string query = "SELECT * FROM user1";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Employee employee = new Employee();
+                        employee.Id = reader.GetInt32(0);
+                        employee.Name = reader.GetString(1);
+                        // Add more properties as needed
+
+                        employees.Add(employee);
+                    }
+                }
+            }
+            string json = JsonConvert.SerializeObject(employees);
+            return Ok(json);
+            //return Ok(employees);
+        }
+
+
+
+
+        public IHttpActionResult Get(int id)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["smsystemconn"].ConnectionString;
+            string query = "SELECT * FROM user1 where id="+id;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+
+                        if (!reader.HasRows)
+                        {
+                            return NotFound();
+                        }
+
+                        Employee employee = new Employee();
+                        employee.Id = reader.GetInt32(0);
+                        employee.Name = reader.GetString(1);
+                        // Add more properties as needed
+
+                        employees.Add(employee);
+                    }
+                }
+            }
+
+            string json = JsonConvert.SerializeObject(employees);
+            return Ok(json);
+
+        }
+
+
+        // POST api/<controller>
+        //public void Post([FromBody] string value)
+        //{
+        //}
+        [HttpPost]
+        // POST: api/employees
+        
+        public IHttpActionResult Post(Employee employee)
+        {
+            try
+            {
+                // Check if the 'Name' property is null or empty
+               // if (string.IsNullOrEmpty(employee.Name))
+               // {
+                    //return BadRequest("Employee name cannot be empty.");
+                //}
+
+                string connectionString = ConfigurationManager.ConnectionStrings["smsystemconn"].ConnectionString;
+                string query = "INSERT INTO user1 ( name) VALUES ( @name)";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        //command.Parameters.AddWithValue("@id", employee.Id);
+                        command.Parameters.AddWithValue("@name", employee.Name);
+
+                        connection.Open();
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            return Ok("Record inserted successfully");
+                        }
+                        else
+                        {
+                            return InternalServerError(new Exception("Failed to insert record"));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // Add more properties as needed
+    }
+    public class Employeess
+    {
+        //public int Id { get; set; }
+        public string Name { get; set; }
+        // Add more properties as needed
+    }
+}
---
+++ trialwebapi/webapihtml.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace trialwebapi
+{
+    public partial class webapihtml : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Your API response string
+                string jsonResponse = "[{\"Id\":1,\"Name\":\"Akash Srivastava\"},{\"Id\":2,\"Name\":\"Monika Srivastava\"},{\"Id\":3,\"Name\":\"Srivastava Ravi\"},{\"Id\":4,\"Name\":\" Srivastava\"},{\"Id\":5,\"Name\":\"Pankaj Srivastava\"}]";
+
+                // Deserialize JSON string into a list of Person objects
+                List<Person> people = JsonConvert.DeserializeObject<List<Person>>(jsonResponse);
+
+                // Bind the list to a GridView
+                GridView1.DataSource = people;
+                GridView1.DataBind();
+            }
+        }
+
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            
+        }
+    }
+}
